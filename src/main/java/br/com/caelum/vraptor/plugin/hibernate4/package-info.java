daeaa7f1a9d9c85953bde1e2637bd947d3e45499
Otávio Garcia <otavio@otavio.com.br>
2014-02-12 12:20:43 -0200

    License info

--- src/main/java/br/com/caelum/vraptor/plugin/hibernate4/package-info.java
+++ src/main/java/br/com/caelum/vraptor/plugin/hibernate4/package-info.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * A simple plugin to provides Hibernate 4.x support to VRaptor.
  */
